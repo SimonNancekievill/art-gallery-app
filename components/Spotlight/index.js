0ed08c7a37e261d8bd95b927168466561fa8129a
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-13 13:09:30 +0200

    same styling for spotlight page

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -1,34 +1,34 @@
 import Link from "next/link";
-import Image from "next/image";
 import FavoriteButton from "@/components/FavoriteButton";
 import { ToggleFavorite } from "@/libs/artPieces";
-import {} from "./Spotlight.styled";
-import { ImageWrap } from "../ArtPiecePreview/ArtPiecePreview.styled";
+import { SpotlightWrapper } from "./Spotlight.styled";
+import {
+  ImageWrap,
+  StyledImage,
+  FavoriteButtonWrap,
+  CardTitle,
+  CardArtist,
+} from "../ArtPiecePreview/ArtPiecePreview.styled";
 
 export default function Spotlight({ artPieces, artPiece, setArtPieces }) {
+  const { imageSource, name, slug, artist } = artPiece;
   return (
-    <>
+    <SpotlightWrapper aria-label="Art Piece Section">
       <ImageWrap>
-        <Link href="/art-pieces">
-          <Image
-            src={artPiece.imageSource}
-            alt={artPiece.name}
-            width="300"
-            height="150"
-            style={{
-              objectFit: "cover",
-            }}
-          />
+        <Link href={`/art-pieces/${slug}`}>
+          <StyledImage src={imageSource} alt={name} fill />
         </Link>
-        <FavoriteButton
-          onToggleFavorite={() =>
-            setArtPieces(ToggleFavorite(artPieces, artPiece))
-          }
-          isFavorite={artPiece.isFavorite}
-          disabled
-        />
+        <FavoriteButtonWrap>
+          <FavoriteButton
+            onToggleFavorite={() =>
+              setArtPieces(ToggleFavorite(artPieces, artPiece))
+            }
+            isFavorite={artPiece.isFavorite}
+          />
+        </FavoriteButtonWrap>
       </ImageWrap>
-      <h2>{artPiece.artist}</h2>
-    </>
+      <CardTitle>{name}</CardTitle>
+      <CardArtist>{artist}</CardArtist>
+    </SpotlightWrapper>
   );
 }
