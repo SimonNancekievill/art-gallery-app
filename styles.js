59b12cf9c7041589a066e97587a1c53c16fd6190
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-13 10:36:54 +0200

    adding two font familys

--- styles.js
+++ styles.js
@@ -9,7 +9,10 @@ export default createGlobalStyle`
 
   body {
     margin: 0;
-    font-family: system-ui;
+     padding:1em 3em 3em;
+    color:#333;
+    background:#eaeaea;
+    font-family: Raleway, sans-serif;
   }
 
   ul {
@@ -38,22 +41,13 @@ export default createGlobalStyle`
   }
 
 
-
-  body {
-    padding:1em 3em 3em;
-    color:#333;
-    background:#eaeaea;
+  a {
+    text-decoration:none;
+    color:#d5561f;
+    margin:.5em 0;
+    display:inline-table;
+    font-family: DBMonoLight, monospace;
   }
-
-  h2 {
-    font-size: 1.75rem;
-  }
-
-  h3 {
-    font-size: 1.5rem;
-  }
-
-  a {text-decoration:none;color:#d5561f;margin:.5em 0;display:inline-table;}
   a:hover {text-decoration:underline;}
 
 
@@ -64,7 +58,29 @@ export default createGlobalStyle`
     background:#333;
   }
 }
+@font-face {
+  font-family: Raleway;
+  src: url("assets/font/raleway/Raleway-VariableFont_wght.ttf") format("truetype");
+  font-weight: 100 900;
+  font-style: normal;
+}
 
-
-
+@font-face {
+  font-family: DBMono;
+  src: url(assets/font/DM_Mono/DMMono-Regular.ttf) format("truetype");
+  font-weight: 500;
+  font-style: normal;
+}
+@font-face {
+  font-family: DBMonoMedium;
+  src: url(assets/font/DM_Mono/DMMono-Medium.ttf) format("truetype");
+  font-weight: 700;
+  font-style: normal;
+}
+@font-face {
+  font-family: DBMonoLight;
+  src: url(assets/font/DM_Mono/DMMono-Light.ttf) format("truetype");
+  font-weight: 300;
+  font-style: normal;
+}
 `;
